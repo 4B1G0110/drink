149d0d0a71e50fadd29aecec5a0bd7ce3a140f5b
4B1G0110 <125246779+4B1G0110@users.noreply.github.com>
2023-05-27 17:30:57 +0800

    Add files via upload

---
+++ 4B1G0110.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+#include <iomanip>
+#include "DrinkItem.h"
+#include "DrinkConsoleApp.h"
+
+int main()
+{
+    vector<DrinkItem> drinks;
+
+    AddNewDrink(drinks);
+    DisplayDrinkMenu(drinks);
+    return 0;
+}
+
+void NewFunction(std::vector<DrinkItem>& drinks)
+{
+    //DrinkItem drink1("紅茶", "大杯", 60);
+    //drinks.push_back(drink1);
+
+    drinks.push_back(DrinkItem("奶茶", "大杯", 60));
+    drinks.push_back(DrinkItem("奶茶", "中杯", 40));
+    drinks.push_back(DrinkItem("紅茶", "大杯", 30));
+    drinks.push_back(DrinkItem("紅茶", "中杯", 20));
+    drinks.push_back(DrinkItem("綠茶", "大杯", 30));
+    drinks.push_back(DrinkItem("綠茶", "中杯", 20));
+    drinks.push_back(DrinkItem("麥茶", "大杯", 30));
+    drinks.push_back(DrinkItem("麥茶", "中杯", 20));
+}
+
+void DisplayDrinkMenu(vector<DrinkItem>& drinks) {
+    cout << setw(4) << "編號" << setw(10) << "飲料名稱" << setw(10) << "大小" << setw(10) << "價格" << endl;
+    cout << "----------------------------------" << endl;
+
+    int count = 1;
+    //for (int i = 0; i < drinks.size(); i++) {
+    //    cout << setw(4) << count;
+    //    drinks[i].displayItem();
+    //    count++;
+    //}
+
+    //for (DrinkItem item : drinks) {
+    //    cout << setw(4) << count;
+    //    item.displayItem();
+    //    count++;
+    //}
+
+    vector<DrinkItem>::iterator v = drinks.begin();
+    while (v != drinks.end()) {
+        cout << setw(4) << count;
+        v->displayItem();
+        v++;
+        count++;
+    }
+
+
+    for (int i = 0; i < drinks.size(); i++) {
+        cout << setw(4) << count;
+        drinks[1].displayItem();
+        count++;
+    }
+
+    cout << "----------------------------------" << endl;
+    cout << "總共有" << drinks.size() << "項飲品" << endl;
+}
